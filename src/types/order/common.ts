7e33fef6a5875cd452ac37cf08c81165aae356dc
kactok <kacpertokarski2000@gmail.com>
2025-12-08 08:57:46 +0100

    chore: add new fetching function

--- src/types/order/common.ts
+++ src/types/order/common.ts
@@ -55,3 +55,17 @@ export interface OrderSet extends Order {
 export type AdminOrderListResponse = PaginatedResponse<{
   orders: AdminOrder[];
 }>;
+
+export interface AdminOrderSetListParams {
+  offset?: number;
+  limit?: number;
+  fields?: string;
+  order_id?: string;
+}
+
+export interface AdminOrderSetListResponse {
+  order_sets: OrderSet[];
+  count?: number;
+  offset?: number;
+  limit?: number;
+}
